b2a945c09ad415d0596c6f1f4f35d6b48d04daba
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-07-19 19:28:54 -0500

    text selection

--- WPF Practice/MainWindow.xaml.cs
+++ WPF Practice/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_Practice
 {
@@ -24,6 +12,20 @@ namespace WPF_Practice
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if(sender != null)
+            {
+                TextBox textBox = sender as TextBox;
+                if (txtStatus != null)
+                {
+                    txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
+                    txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
+                    txtStatus.Text += "Selection text: '" + textBox.SelectedText + "'.";
+                }
+            }
         }
     }
 }
