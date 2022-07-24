9e79fe55bf58b44142c838bf077c37ba2d99cb11
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-07-24 13:01:28 -0500

    custom control

--- WPF Practice/MainWindow.xaml.cs
+++ WPF Practice/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace WPF_Practice
 {
@@ -12,20 +10,6 @@ namespace WPF_Practice
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            if(sender != null)
-            {
-                TextBox textBox = sender as TextBox;
-                if (txtStatus != null)
-                {
-                    txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
-                    txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
-                    txtStatus.Text += "Selection text: '" + textBox.SelectedText + "'.";
-                }
-            }
         }
     }
 }
