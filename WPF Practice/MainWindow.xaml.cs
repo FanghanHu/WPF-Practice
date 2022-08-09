25ae1c4f64958c568b22a13510a46482ee5c1f64
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-08-09 11:02:50 -0500

    ValueConverters

--- WPF Practice/MainWindow.xaml.cs
+++ WPF Practice/MainWindow.xaml.cs
@@ -9,63 +9,9 @@ namespace WPF_Practice
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<User> users = new ObservableCollection<User>();
-
         public MainWindow()
         {
             InitializeComponent();
-
-            users.Add(new User() { Name = "John Doe"});
-            users.Add(new User() { Name = "Jane Doe" });
-
-            lbUsers.ItemsSource = users;
         }
-
-        private void btnAddUser_Click(object sender, RoutedEventArgs e)
-        {
-            users.Add(new User() { Name = "New User" });
-        }
-
-        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
-        {
-            if(lbUsers.SelectedItem != null)
-            {
-                (lbUsers.SelectedItem as User).Name = "New Name";
-            }
-        }
-
-        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
-        {
-            if (lbUsers.SelectedItem != null)
-            {
-                users.Remove(lbUsers.SelectedItem as User);
-            }
-        }
-    }
-
-    public class User : INotifyPropertyChanged
-    {
-        private string name;
-        public string Name
-        {
-            get
-            {
-                return this.name;
-            }
-
-            set
-            {
-                if(this.name != value)
-                {
-                    this.name = value;
-                    if(this.PropertyChanged != null)
-                    {
-                        this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
-                    }
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ WPF Practice/YesNoBooleanConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Practice
+{
+    public class YesNoBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch(value.ToString().ToLower())
+            {
+                case "yes":
+                case "yeah":
+                case "correct":
+                    return true;
+                case "no":
+                case "nope":
+                    return false;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if ((bool)value == true)
+                    return "yes";
+                else
+                    return "no";
+            }
+
+            return "no";
+        }
+    }
+}
